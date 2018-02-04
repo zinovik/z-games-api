3587d9c6210bd21d6668d1a9183715a8cabb37ff
Max Zinovik <zinovik@gmail.com>
2018-02-04 16:58:46 +0300

    Added console.log instead of debug

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var session = require('./session');
 
 var app = express();
+
+require('dotenv').load();
+
+fs.writeFile(__dirname + '/public/index.html', `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Games Server</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+      window.location.href = "${process.env.clientURL || 'http://localhost:8100'}";
+    </script>
+  </head>
+  <body>
+  </body>
+</html>`, function(err) {
+  if (err) {
+    return console.error(err);
+  }
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
