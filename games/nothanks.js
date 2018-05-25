e0380477482bdfee51ef3368faecf36f6c657c8a
Max Zinovik <zinovik@gmail.com>
2018-05-25 07:49:16 +0300

    Fixed place sorting

--- games/nothanks.js
+++ games/nothanks.js
@@ -187,8 +187,8 @@ class NoThanks {
     }
 
     playersPlaces.sort((a, b) => {
-      if (a.points > b.points) return 1;
-      if (a.points < b.points) return -1;
+      if (a.points > b.points) return -1;
+      if (a.points < b.points) return 1;
       return 0;
     });
 
