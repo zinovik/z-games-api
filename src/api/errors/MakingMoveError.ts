851d0dfa4d25a7ae7c4f0276f683abfa472bfd4c
Max Zinovik <zinovik@gmail.com>
2019-01-26 10:25:29 +0300

    Initial nest.js commit

--- src/api/errors/MakingMoveError.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestError } from 'routing-controllers';
-
-export class MakingMoveError extends BadRequestError {
-  constructor(message: string) {
-    super(`Error making a move: ${message}`);
-  }
-}
--- src/api/errors/StartingGameError.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestError } from 'routing-controllers';
-
-export class StartingGameError extends BadRequestError {
-  constructor(message: string) {
-    super(`Error starting game: ${message}`);
-  }
-}
