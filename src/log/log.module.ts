2ab34582917191fde969c4ed266f34abbb6d4dc6
Max Zinovik <zinovik@gmail.com>
2019-02-12 19:24:27 +0300

    add mongo schema in each module

--- src/log/log.module.ts
+++ src/log/log.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserModule } from './../user/user.module';
+import { ServicesModule } from './../services/services.module';
 import { LogGateway } from './log.gateway';
 import { LogController } from './log.controller';
 import { LogService } from './log.service';
 import { LoggerModule } from '../logger/logger.module';
+import { ConfigService } from '../config/config.service';
+import { LogSchema } from '../db/schemas/log.schema';
+
+const additionalModules = [];
+if (ConfigService.get().USE_MONGO === 'true') {
+  additionalModules.push(MongooseModule.forFeature([{ name: 'Log', schema: LogSchema }]));
+}
 
 @Module({
-  imports: [LoggerModule, UserModule],
+  imports: [ServicesModule, LoggerModule, UserModule, ...additionalModules],
   controllers: [LogController],
   providers: [LogGateway, LogService],
   exports: [LogService],
