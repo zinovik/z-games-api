cd782c46dcc8a62479af46193f5feccfa8945c1b
Max Zinovik <zinovik@gmail.com>
2019-02-12 19:24:27 +0300

    add mongo schema in each module

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { LogModule } from '../log/log.module';
 import { UserModule } from './../user/user.module';
 import { ServicesModule } from './../services/services.module';
+import { ConfigModule } from '../config/config.module';
 import { LoggerModule } from './../logger/logger.module';
 import { GameController } from './game.controller';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
+import { ConfigService } from '../config/config.service';
+import { GameSchema } from '../db/schemas/game.schema';
+
+const additionalModules = [];
+if (ConfigService.get().USE_MONGO === 'true') {
+  additionalModules.push(MongooseModule.forFeature([{ name: 'Game', schema: GameSchema }]));
+}
 
 @Module({
-  imports: [LogModule, UserModule, ServicesModule, LoggerModule],
+  imports: [
+    ConfigModule,
+    LogModule,
+    UserModule,
+    ServicesModule,
+    LoggerModule,
+    ...additionalModules,
+  ],
   controllers: [GameController],
   providers: [GameGateway, GameService],
 })
