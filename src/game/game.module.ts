fba74cc4bf5f59cf4fdf7d6af627e6a125778bd1
Max Zinovik <zinovik@gmail.com>
2019-02-13 21:54:51 +0300

    update mongodb models

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { LogModule } from '../log/log.module';
 import { UserModule } from './../user/user.module';
@@ -9,13 +8,6 @@ import { LoggerModule } from './../logger/logger.module';
 import { GameController } from './game.controller';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
-import { ConfigService } from '../config/config.service';
-import { GameSchema } from '../db/schemas/game.schema';
-
-const additionalModules = [];
-if (ConfigService.get().USE_MONGO === 'true') {
-  additionalModules.push(MongooseModule.forFeature([{ name: 'Game', schema: GameSchema }]));
-}
 
 @Module({
   imports: [
@@ -24,7 +16,6 @@ if (ConfigService.get().USE_MONGO === 'true') {
     UserModule,
     ServicesModule,
     LoggerModule,
-    ...additionalModules,
   ],
   controllers: [GameController],
   providers: [GameGateway, GameService],
