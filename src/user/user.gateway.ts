d1d30b6ee3c4034819aa152ba8cb664dd3d0e63e
Max Zinovik <zinovik@gmail.com>
2019-03-21 18:12:39 +0300

    update users online

--- src/user/user.gateway.ts
+++ src/user/user.gateway.ts
@@ -8,12 +8,7 @@ import { UseGuards } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 
 import { JwtGuard } from '../guards/jwt.guard';
-import { JwtService } from '../services/jwt.service';
-import { UserService } from '../user/user.service';
-// import { LogService } from '../log/log.service';
-// import { GameService } from '../game/game.service';
 import { User } from '../db/entities/user.entity';
-import { Game } from '../db/entities/game.entity';
 
 @WebSocketGateway()
 export class UserGateway {
@@ -21,155 +16,56 @@ export class UserGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly userService: UserService,
-    // private readonly logService: LogService,
-    // private readonly gameService: GameService,
-  ) { }
-
-  private disconnectTimers: { [key: string]: NodeJS.Timeout } = {};
-  private connectTimers: { [key: string]: NodeJS.Timeout } = {};
-
-  async handleConnection(client: Socket) {
-    const token = client.handshake.query.token;
-
-    if (this.connectTimers[token]) {
-      return;
-    }
-
-    this.connectTimers[token] = setTimeout(async () => {
-      delete this.connectTimers[token];
-    }, 4000);
-
-    const userId = this.jwtService.getUserIdByToken(token);
-
-    const user = await this.userService.findOneByUserId(userId);
-
-    if (user && this.disconnectTimers[user.id]) {
-      clearTimeout(this.disconnectTimers[user.id]);
-      delete this.disconnectTimers[user.id];
-      return;
-    }
-
-    this.updateUsersOnline(user);
+  handlConnect() {
+    this.updateUsersOnline();
   }
 
-  async handleDisconnect(client: Socket) {
-    const token = client.handshake.query.token;
-
-    const userId = this.jwtService.getUserIdByToken(token);
-
-    const user = await this.userService.findOneByUserId(userId);
-
-    if (!user) {
-      return;
-    }
-
-    this.disconnectTimers[user.id] = setTimeout(async () => {
-      this.updateUsersOnline();
-      delete this.disconnectTimers[user.id];
-    }, 4000);
+  handleDisconnect() {
+    this.updateUsersOnline();
   }
 
   @UseGuards(JwtGuard)
   @SubscribeMessage('get-current-user')
   public getCurrentUser(
     client: Socket & { user: User },
-    payload: void,
   ): WsResponse<User> {
     return { event: 'update-current-user', data: client.user };
   }
 
-  @UseGuards(JwtGuard)
   @SubscribeMessage('get-users-online')
-  public getUsersOnline(
-    client: Socket & { user: User },
-    payload: void,
-  ): WsResponse<User[]> {
-    return { event: 'update-users-online', data: this.checkUsersOnline(client.user) };
+  public getUsersOnline(): void {
+    this.updateUsersOnline();
   }
 
   @UseGuards(JwtGuard)
   @SubscribeMessage('logout')
-  public async logout(client: Socket & { user: User }, payload: void): Promise<WsResponse<null>> {
-    // const token = client.handshake.query.token;
-
-    // const userId = this.jwtService.getUserIdByToken(token);
-
-    // const user = await this.userService.findOneByUserId(userId);
-
-    // if (user && user.openedGame) {
-    //   const log = await this.logService.create({
-    //     type: 'logout',
-    //     user: client.user,
-    //     gameId: user.openedGame.id,
-    //   });
-    //   user.openedGame.logs = [log, ...user.openedGame.logs];
-
-    //   client.leave(user.openedGame.id);
-
-    //   this.sendGameToGameUsers({ server: this.server, game: user.openedGame });
-    //   this.server.emit(
-    //     'update-game',
-    //     this.gameService.parseGameForAllUsers(user.openedGame),
-    //   );
-    // }
-
+  public async logout(client: Socket & { user: User }): Promise<WsResponse<null>> {
     client.emit('new-token', null);
     return { event: 'update-current-user', data: null };
   }
 
-  checkUsersOnline(currentUser?: User) {
-    const usersOnline: User[] = [];
+  private getUsersOnlineFromSockets(sockets: { [key: string]: Socket }): {
+    users: User[],
+    usersCount: number,
+  } {
+    const users: User[] = [];
 
-    if (currentUser) {
-      usersOnline.push(currentUser);
-    }
+    Object.keys(sockets).forEach(socketId => {
+      const socket = sockets[socketId] as Socket & { user: User };
 
-    Object.keys(this.server.sockets.connected).forEach((socketId: string) => {
-      const socket = this.server.sockets.connected[
-        socketId
-      ] as Socket & { user: User };
-
-      const user = socket.user;
-
-      if (!user) {
-        return;
-      }
-
-      if (!currentUser || user.id !== currentUser.id) {
-        usersOnline.push(user);
+      if (socket.user) {
+        users.push(socket.user);
       }
     });
 
-    return usersOnline;
+    return {
+      users,
+      usersCount: Object.keys(sockets).length,
+    };
   }
 
-  updateUsersOnline(currentUser?: User) {
-    this.server.emit('update-users-online', this.checkUsersOnline(currentUser));
+  updateUsersOnline() {
+    this.server.emit('update-users-online', this.getUsersOnlineFromSockets(this.server.sockets.connected));
   }
 
-  // TODO: Move to the game service
-  // private sendGameToGameUsers({ server, game }: { server: Server, game: Game }): void {
-  //   if (!server.sockets.adapter.rooms[game.id]) {
-  //     return;
-  //   }
-
-  //   Object.keys(server.sockets.adapter.rooms[game.id].sockets).forEach(
-  //     (socketId: string) => {
-  //       const socketInGame = server.sockets.connected[
-  //         socketId
-  //       ] as Socket & { user: User };
-  //       const userInGame = socketInGame.user;
-
-  //       if (userInGame) {
-  //         socketInGame.emit(
-  //           'update-opened-game',
-  //           this.gameService.parseGameForUser({ game, user: userInGame }),
-  //         );
-  //       }
-  //     },
-  //   );
-  // }
 }
