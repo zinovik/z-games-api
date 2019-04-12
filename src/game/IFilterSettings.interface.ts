a07711272b0fdb1af2a35ae03dbe7cb048e6ccd5
Max Zinovik <zinovik@gmail.com>
2019-04-12 17:34:36 +0300

    add game joining after game creation

---
+++ src/game/IFilterSettings.interface.ts
@@ -0,0 +1,14 @@
+export interface IFilterSettings {
+	isNotStarted: boolean;
+	isStarted: boolean;
+	isFinished: boolean;
+	isWithMe: boolean;
+	isWithoutMe: boolean;
+	isMyMove: boolean;
+  isNotMyMove: boolean;
+	isGames: {
+		[key: string]: boolean;
+	};
+	limit: number;
+	offset: number;
+}
