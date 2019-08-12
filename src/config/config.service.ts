1497ecfc22ce5cf5ce35ee8c2c9d3cb57f95d993
Max Zinovik <zinovik@gmail.com>
2019-08-12 19:19:04 +0300

    fix test variables

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ConfigService {
   static get() {
-    dotenv.config();
+    if (!process.env.JEST_WORKER_ID) {
+      dotenv.config();
+    }
 
     if (!process.env.DATABASE_URL && !process.env.MONGODB_URI) {
       process.env.IS_MONGO_USED = 'true';
